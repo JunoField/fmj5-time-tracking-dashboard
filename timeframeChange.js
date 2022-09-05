69db433793b227b5f6580aa2c8a675b83bc1116e
JunoField <junofield123@gmail.com>
2022-09-05 21:12:24 +0100

    cleaned up timeframeChange js

--- timeframeChange.js
+++ timeframeChange.js
@@ -1,31 +1,28 @@
-document.getElementById("button-daily").addEventListener("click", () => {
-    timeSelect("day");
-    document.getElementById("button-daily").classList.add("time-active");
-});
-document.getElementById("button-weekly").addEventListener("click", () => {
-    timeSelect("week");
-    document.getElementById("button-weekly").classList.add("time-active");
-});
-document.getElementById("button-monthly").addEventListener("click", () => {
-    timeSelect("month");
-    document.getElementById("button-monthly").classList.add("time-active");
-});
+//add event listeners for button objects
+document.getElementById("button-day").addEventListener("click", () => {timeSelect("day");});
+document.getElementById("button-week").addEventListener("click", () => {timeSelect("week");});
+document.getElementById("button-month").addEventListener("click", () => {timeSelect("month");});
 
+
+//when a time frame is selected:
 function timeSelect(timeFrame){
-    console.log("time clicked");
+    //hide all existing times
     let elementsToHide = document.querySelectorAll(".time-current, .time-previous");
     elementsToHide.forEach(element => {
         element.classList.add("hidden");
     });
     
-    
+    //show times that have just been enabled
     let elementsToShow = document.querySelectorAll(".current-" + timeFrame + ", .previous-" + timeFrame);
     elementsToShow.forEach(element => {
         element.classList.remove("hidden");
     });
 
+    //remove active state from buttons
     document.querySelectorAll(".time-button").forEach(element => {
         element.classList.remove("time-active");
     })
+    //apply active state to applied button
+    document.getElementById("button-" + timeFrame).classList.add("time-active");
 
 }
