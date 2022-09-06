3e768a56cf27953751bb681030682606e0c4b285
JunoField <junofield123@gmail.com>
2022-09-06 15:34:44 +0100

    added "last x" to import display

--- dataImport.js
+++ dataImport.js
@@ -5,15 +5,15 @@ async function loadData(){
         console.log(data);
 
         for (var section of data){
-            let sectionName = section.title.toLowerCase();
+            let sectionName = section.title.toLowerCase().replace(/ /g,"-");
 
             document.getElementById(sectionName + "-current-day").innerHTML = section.timeframes.daily.current;
             document.getElementById(sectionName + "-current-week").innerHTML = section.timeframes.weekly.current;
             document.getElementById(sectionName + "-current-month").innerHTML = section.timeframes.monthly.current;
 
-            document.getElementById(sectionName + "-previous-day").innerHTML = section.timeframes.daily.previous;
-            document.getElementById(sectionName + "-previous-week").innerHTML = section.timeframes.weekly.previous;
-            document.getElementById(sectionName + "-previous-month").innerHTML = section.timeframes.monthly.previous;
+            document.getElementById(sectionName + "-previous-day").innerHTML = "Yesterday - " + section.timeframes.daily.previous;
+            document.getElementById(sectionName + "-previous-week").innerHTML = "Last Week - " + section.timeframes.weekly.previous;
+            document.getElementById(sectionName + "-previous-month").innerHTML = "Last Month - " + section.timeframes.monthly.previous;
 
         };
     })
